75f0c2870e3b73312dfef0c36013cf4b8d031c7d
frankenzy.doskiller <frankenzyvenus@gmail.com>
2026-03-11 09:04:42 +0000

    feat: add initial messages array for chat functionality

---
+++ src/data/messages.ts
@@ -0,0 +1,8 @@
+export const messages = [
+   "Salut les filles 😂",
+   "Qui travaille à Cocody ?",
+   "Moi je suis à Marcory",
+   "Le bébé dort enfin 😅",
+   "Qui est libre ce soir ?",
+   "Le bébé m'a fatiguée aujourd'hui 😂"
+]
